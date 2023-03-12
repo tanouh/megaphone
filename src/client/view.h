18444e05fe7529ddd77bf76bc040d5e158204c64
Lucas Ivan <ivan.lucasreiner@gmail.com>
2023-03-12 02:32:08 +0100

    Clean + complete view.h

--- src/client/view.h
+++ src/client/view.h
@@ -5,9 +5,25 @@
 
 char *ask_pseudo();
 
+void welcome(int already_registered);
+
 void show_id(uint16_t id);
 
 char *choose_action();
+
+char *choose_thread_to_post_in();
+
+char *post_ticket(char *thread);
+
+char *add_file();
+
+char *select_thread_to_unfold();
+
+char *see_previous_tickets(char *thread);
+
+char *download_file();
+
+char *subscribe_to_thread();
 
 void print_error(char *error);
 
