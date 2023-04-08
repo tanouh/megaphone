31fc02c4d97ac3c33562e4f213541ef4d71075fb
Tania <taniamahandry24@gmail.com>
2023-04-08 23:42:23 +0200

    nettoyage (correction en cours)

--- tests/server/test_chat.c
+++ tests/server/test_chat.c
@@ -7,20 +7,16 @@
 #include "../printlib.h"
 #include "../test_constants.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 static int test_add_ticket_to_chat();
 //static int test_get_chat();
 
-// static int cmpc (uint16_t *a, struct chat *b)
-// {
-// 	if(*a == b->id){
-// 		return 0;
-// 	}
-// 	return (*a < b->id) ? 1 : -1;
-// }
-
 static int cmptck (struct ticket *t1, struct ticket *t2)
 {
+	if(t1->feed == NULL || t2->feed == NULL) {
+		return 0;
+	}
 	if ((t1->owner != t2->owner) || (t1->feed->id != t2->feed->id) 
 	|| (t1->isFile != t2->isFile) || (t1->datalen != t2->datalen)){
 		return 0;
@@ -59,20 +55,3 @@ static int test_add_ticket_to_chat()
 	free(t2);
 	return ret;
 }
-// static int test_get_chat()
-// {
-// 	truct chat *c = build_chat();
-// 	struct ticket *t1 = build_ticket(USER,NULL,TEXT_SIZE,TEXT,ISNOTFILE);
-// 	add_tickets_to_chat(c,t1);
-// 	struct ticket *t2 = build_ticket(USER,NULL,TEXT_SIZE,TEXT,ISNOTFILE);
-// 	add_tickets_to_chat(c,t2);
-// 	int ret =1;
-
-
-// 	free(c);
-// 	free(t1);
-// 	free(t2);
-// 	return ret;
-
-
-// }
